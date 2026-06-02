108af28ccdd98a57572cc62bb1dc48cba9ade4e8
Maria Magnell <maria.magnell@gmail.com>
2026-06-02 20:52:01 +0200

    CRUD for instructors

---
+++ Application/Modules/Instructors/IInstructorService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Modules.Instructors.Inputs;
+using Application.Modules.Instructors.Outputs;
+
+namespace Application.Modules.Instructors;
+
+public interface IInstructorService
+{
+    InstructorOutput Create(CreateInstructorInput input);
+    bool Delete(Guid id);
+    InstructorOutput? GetById(Guid id);
+    IEnumerable<InstructorOutput> GetAll();
+    InstructorOutput? Update(UpdateInstructorInput input);
+}
---
+++ Application/Modules/Instructors/Inputs/CreateInstructorInput.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Modules.Instructors.Inputs;
+
+public record CreateInstructorInput
+(
+    string FirstName,
+    string LastName,
+    string Title,
+    string? Description
+);
