26a42ace44e3ebd67c8d4955cad44e3675b9500b
Maria Magnell <maria.magnell@gmail.com>
2026-06-10 15:17:18 +0200

    added scalar documention

---
+++ shiko-instructor-provider-api/OpenApi/OpenApiDocumentTransformer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace shiko_instructor_provider_api.OpenApi;
+
+public sealed class OpenApiDocumentTransformer : IOpenApiDocumentTransformer
+{
+    public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
