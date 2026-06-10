78f8124e633d98cb69947b98f865498b88fd5b51
Maria Magnell <maria.magnell@gmail.com>
2026-06-10 15:00:27 +0200

    Added CORS

---
+++ shiko-instructor-provider-api/Security/CorsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace shiko_instructor_provider_api.Security;
+
+public static class CorsConfiguration
+{
+    public static IServiceCollection AddCorsConfiguration(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("All", policy =>
+            {
+                policy
+                    .WithOrigins("http://127.0.0.1:5500", "http://localhost:3000", "https://localhost:3000")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
+        });
+
+        return services;
+    }
+}
